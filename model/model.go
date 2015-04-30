aed1f2dd4c49deed84f5ff3690b26692a830a674
Eric Mendes Dantas <ericdantas0@gmail.com>
2015-04-30 07:25:27 -0300

    ..

--- model/model.go
+++ model/model.go
@@ -3,7 +3,7 @@ package model
 import "gopkg.in/mgo.v2/bson"
 
 type Info struct {
-	Id            bson.ObjectId `json="id" bson="_id"`
-	Email         string        `json="email" bson="email"`
-	IdAlgumaCoisa string        `json="idAlgumaCoisa" bson="idAlgumaCoisa"`
+	Id            bson.ObjectId `json:"id" bson:"_id"`
+	Email         string        `json:"email" bson:"email"`
+	IdAlgumaCoisa string        `json:"idAlgumaCoisa" bson:"idAlgumaCoisa"`
 }
